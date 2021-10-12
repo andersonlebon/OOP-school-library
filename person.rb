eacedc2c2c880fe5ef53320e01e044b95d61febd
Caleb Anderson <buyananderson@gmail.com>
2021-10-12 21:53:52 +0200

    Correct linter errors  in all ruby files

--- person.rb
+++ person.rb
@@ -1,5 +1,4 @@
 class Person
-    
   attr_accessor :name, :age
   attr_reader :id
 
@@ -9,6 +8,7 @@ class Person
     @age = age
     @parent_permission = parent_permission
   end
+
   def of_age
     @age > 18
   end
