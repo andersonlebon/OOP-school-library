883f5459d173dc33b4f31a8a5ebf80a76f6e7db0
Caleb Anderson <buyananderson@gmail.com>
2021-10-12 15:47:20 +0200

    Correct linter errors  in all ruby files

--- person.rb
+++ person.rb
@@ -1,28 +1,24 @@
 class Person
-    def initialize(age, name="Unknown", parent_permission=true)
-        @id = Random.rand(1..1000)
-        @name = name
-        @age = age
-        @parent_permission = parent_permission
-    end
-    attr_reader :id,:name,:age
-    attr_writer :name,:age
+  def initialize(age, name = 'Unknown', parent_permission: true)
+    @id = Random.rand(1..1000)
+    @name = name
+    @age = age
+    @parent_permission = parent_permission
+  end
+  attr_accessor :name, :age
+  attr_reader :id
 
-    def is_of_age
-        if @age >18
-            true
-        else
-            false
-        end
-    end
+  def of_age
+    @age > 18
+  end
 
-    def can_use_services
-        if age >= 18 || parent_permission
-            true
-        else
-            false
-        end        
+  def can_use_services
+    if age >= 18 || parent_permission
+      true
+    else
+      false
     end
+  end
 
-    private :is_of_age
+  private :of_age
 end
