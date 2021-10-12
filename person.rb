f9eb4e0ed76dedd7c7e108c7a14897208bf8f770
Caleb Anderson <buyananderson@gmail.com>
2021-10-12 14:20:38 +0200

    Add methods in the Person class

--- person.rb
+++ person.rb
@@ -1,5 +1,28 @@
 class Person
-    def initialize(name="Unknown", age, parent_permission = "Unknown")
-        
+    def initialize(name="Unknown", age, parent_permission = true)
+        @id = Random.rand(1..1000)
+        @name = name
+        @age = age
+        @parent_permission = parent_permission
     end
+    attr_reader :id,:name,:age
+    attr_writer :name,:age
+
+    def is_of_age
+        if @age >18
+            true
+        else
+            false
+        end
+    end
+
+    def can_use_services
+        if age >= 18 || parent_permission
+            true
+        else
+            false
+        end        
+    end
+
+    private is_of_age
 end
