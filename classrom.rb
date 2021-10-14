edf39b7db751dce51d8976d95cfd1f968f1af921
Caleb Anderson <buyananderson@gmail.com>
2021-10-14 12:22:45 +0200

    Add a method in classroom

--- classrom.rb
+++ classrom.rb
@@ -1,5 +1,14 @@
 class Classroom
-    def initialize(label)
-        @label = label
-    end
+  attr_reader :students
+  attr_accessor :label
+
+  def initialize(label)
+    @label = label
+    @students = []
+  end
+
+  def add_student(student)
+    @students << student
+    student.classroom = self
+  end
 end
