9744c9b03ba68d61d564c79ee95bba29a0e3134a
Caleb Anderson <buyananderson@gmail.com>
2021-10-15 16:31:30 +0200

    Create cases when an option was selected

--- main.rb
+++ main.rb
@@ -20,6 +20,20 @@ def say_welcome
 end
 
 
+def selected_option(option)
+    case option
+    when "1"
+        1
+    when "2"
+        2
+    when "3"
+       3 
+    else 
+        4
+    end
+end
+
+
 
 welcome_message = ["| Hello welcome to OOP-SCHOOL-LIBRARY App |" , "| Please choose an option by entering a number |"]
 options = ["1:  -  List all books", "2:  -  List all people", "3:  -  Create a person (teacher or student)", "4:  -  Create a book", "5:  -  Create a rental", "6:  -  List all rentals for a given person id"]
