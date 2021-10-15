30507425d2dbbfacb88c58f542607a206e94d194
Caleb Anderson <buyananderson@gmail.com>
2021-10-15 16:35:04 +0200

    Use the main method to say welcom

--- main.rb
+++ main.rb
@@ -7,16 +7,30 @@ require './teacher.rb'
 
 
 class Storage
-    attr_reader : books
+    attr_reader :books
     def initialize
         @books = []
         @peolpe = []       
     end
 end
 def say_welcome
+    welcome_message = ["| Hello welcome to OOP-SCHOOL-LIBRARY App |" , "| Please choose an option by entering a number |"]
+    options = ["1:  -  List all books", "2:  -  List all people", "3:  -  Create a person (teacher or student)", "4:  -  Create a book", "5:  -  Create a rental", "6:  -  List all rentals for a given person id"]
+    puts "\n"*3
 
+    puts  "="* 43
+    puts "|" + " "* 41 + "|"
+    puts welcome_message[0].upcase
+    puts "|" + " "* 41 + "|"
+    puts  "="* 43
+    puts "\n"*3
 
+    puts  "-"*49
+    puts welcome_message[1]
+    puts  "-"* 49
+    puts "\n"*2
 
+    puts options
 end
 
 
@@ -34,21 +48,9 @@ def selected_option(option)
 end
 
 
+def main
+    say_welcome()
+end
 
-welcome_message = ["| Hello welcome to OOP-SCHOOL-LIBRARY App |" , "| Please choose an option by entering a number |"]
-options = ["1:  -  List all books", "2:  -  List all people", "3:  -  Create a person (teacher or student)", "4:  -  Create a book", "5:  -  Create a rental", "6:  -  List all rentals for a given person id"]
-puts "\n"*3
 
-puts  "="* 43
-puts "|" + " "* 41 + "|"
-puts welcome_message[0].upcase
-puts "|" + " "* 41 + "|"
-puts  "="* 43
-puts "\n"*3
-
-puts  "-"*49
-puts welcome_message[1]
-puts  "-"* 49
-puts "\n"*2
-
-puts options
+main()
