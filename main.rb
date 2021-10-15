c70bd796e38b91ba9fdfaa4eec04f35e79264ec9
Caleb Anderson <buyananderson@gmail.com>
2021-10-15 16:23:08 +0200

    Add selected options

--- main.rb
+++ main.rb
@@ -10,7 +10,7 @@ def say_welcome
 
 end
 welcome_message = ["| Hello welcome to OOP-SCHOOL-LIBRARY App |" , "| Please choose an option by entering a number |"]
-options = ["1: List all books"]
+options = ["1:  -  List all books", "2:  -  List all people", "3:  -  Create a person (teacher or student)", "4:  -  Create a book", "5:  -  Create a rental", "6:  -  List all rentals for a given person id"]
 puts "\n"*3
 
 puts  "="* 43
@@ -23,3 +23,6 @@ puts "\n"*3
 puts  "-"*49
 puts welcome_message[1]
 puts  "-"* 49
+puts "\n"*2
+
+puts options
