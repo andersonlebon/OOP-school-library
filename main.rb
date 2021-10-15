b8a04028226fc40c6b1bdc09dc2bb09768f34575
Caleb Anderson <buyananderson@gmail.com>
2021-10-15 16:07:22 +0200

    Decorate the welcome message

--- main.rb
+++ main.rb
@@ -3,3 +3,21 @@ require './classroom.rb'
 require './book.rb'
 require './student.rb'
 require './teacher.rb'
+
+
+def createData
+
+
+end
+welcome_message = ["| Hello welcome to OOP-SCHOOL-LIBRARY App |" , "| Please chose an option by entering a number as option |"]
+options = ["1: "]
+puts  "="* 43
+puts "|" + " "* 41 + "|"
+puts welcome_message[0].upcase
+puts "|" + " "* 41 + "|"
+puts  "="* 43
+puts "|"
+
+puts  "-"*57
+puts welcome_message[1]
+puts  "-"* 57
